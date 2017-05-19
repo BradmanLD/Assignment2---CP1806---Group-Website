68b532326c2ab228e0a30b09cd85ab108191e9ca
Bradman Davis <bradman.davis@my.jcu.edu.au>
2017-05-19 11:29:46 +1000

    Add a template page, Move required files into MILESTONE 2, Reorganize files,

---
+++ MILESTONE 2/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.sponsors').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: true,
+    });
+});
