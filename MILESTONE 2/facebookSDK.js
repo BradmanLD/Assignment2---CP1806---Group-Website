c844a41d3d05c01d16b12c3b55ed51ccd7da52c7
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-05-30 11:22:48 +1000

    Fix links, add password to membership form

--- MILESTONE 2/facebookSDK.js
+++ MILESTONE 2/facebookSDK.js
@@ -5,11 +5,12 @@ window.fbAsyncInit = function() {
         xfbml      : true,
         version    : 'v2.4'
     });
-
+}
+	
 (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
+	var js, fjs = d.getElementsByTagName(s)[0];
+  	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+  	js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
+	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
